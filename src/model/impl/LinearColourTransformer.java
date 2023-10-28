df2675c0485ac1d188b020f1a3d6935c6edc7860
gibranmyageri <myageri.g@northeastern.edu>
2023-10-28 19:10:12 -0400

    Added implementation classes for transformer and processor
    Signed-off-by: gibranmyageri <myageri.g@northeastern.edu>

---
+++ src/model/impl/LinearColourTransformer.java
@@ -0,0 +1,17 @@
+package model.impl;
+
+import model.intf.ColorTransformer;
+import model.intf.ImageModel;
+
+public class LinearColourTransformer implements ColorTransformer {
+
+  @Override
+  public ImageModel greyScale(ImageModel image) {
+    return null;
+  }
+
+  @Override
+  public ImageModel sepia(ImageModel image) {
+    return null;
+  }
+}
--- src/model/impl/RGBImageProcessor.java
+++ src/model/impl/RGBImageProcessor.java
@@ -1,5 +1,47 @@
 package model.impl;
 
-public class RGBImageProcessor {
+import model.intf.ImageModel;
+import model.intf.ImageProcessor;
 
+public class RGBImageProcessor implements ImageProcessor {
+
+  @Override
+  public ImageModel brighten(ImageModel image, int factor) {
+    return null;
+  }
+
+  @Override
+  public ImageModel blur(ImageModel image) {
+    return null;
+  }
+
+  @Override
+  public ImageModel sharpen(ImageModel image) {
+    return null;
+  }
+
+  @Override
+  public ImageModel horizontalFlip(ImageModel image) {
+    return null;
+  }
+
+  @Override
+  public ImageModel verticalFlip(ImageModel image) {
+    return null;
+  }
+
+  @Override
+  public int[][] getRedComponent(ImageModel image) {
+    return new int[0][];
+  }
+
+  @Override
+  public int[][] getGreenComponent(ImageModel image) {
+    return new int[0][];
+  }
+
+  @Override
+  public int[][] getBlueComponent(ImageModel image) {
+    return new int[0][];
+  }
 }
